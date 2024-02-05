d393ff88773c00bfd7dc3897869bb4e1e6d2a23c
sakshikalyankar22 <sakshipk01@gmail.com>
2024-02-06 00:18:25 +0530

    Auth

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env : {
+        GITHUB_ID:"8cd3dcf56738d47ed46c",
+GITHUB_SECRET:"2e4cce27085025d4c1868bf7d6d185721c87a7f6",
+NEXTAUTH_SECRET:"something"
+    },
+};
 
 export default nextConfig;
